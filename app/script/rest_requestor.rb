2c87a496dc3d96f4907ba179e4701394d1bae214
codingAngarita <https://codingangarita@github.com>
2019-07-22 12:14:29 -0500

    Route setup modified

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -1,7 +1,7 @@
 require 'rest-client'
 
 puts "Enter URL:\nLeave blank for default"
-url = gets.chomp
-url = "http://localhost:3000" if url == ""
+urlAdd = gets.chomp
+url = "http://localhost:3000/" + urlAdd
 
 puts RestClient.get(url)
